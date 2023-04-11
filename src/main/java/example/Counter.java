a90b43ee4a9aa341410531eaeb70c3f65e1d8ed1
Walt McDonald <walrussin@mailfence.com>
2023-04-10 19:52:09 -0600

    intial commit

---
+++ src/main/java/example/Counter.java
@@ -0,0 +1,26 @@
+package example;
+
+public class Counter implements Runnable{
+    private int val = 0;  
+    public void increment() {   
+        val++;  
+    }
+
+    public void decrement() {      
+        val--;  
+    }  
+    public int getValue() {  
+        return val;  
+    }  
+    
+    @Override  
+    public void run() {
+        //incrementing  
+        this.increment();  
+        System.out.println("Value for Thread after increment: " + Thread.currentThread().getName() + " " + this.getValue());  
+        //decrementing  
+        this.decrement();  
+        System.out.println("Value for Thread after decrement: " + Thread.currentThread().getName() + " " + this.getValue());          
+    }  
+}  
+
